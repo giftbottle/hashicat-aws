724dd0e0845b0ace8eab30e0fec9d9a39b8122f9
Robert Kaprinyak <robertkaprinyak@gmail.com>
2022-04-27 19:10:00 +0000

    Fixed s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/robert-cdw-tfc-aws-labs/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
-  bucket_prefix = var.prefix
-  acl    = "private"
+  bucket_prefix = var.prefix"
 }
